2f2541f864265e9ab24c850451ffb03718ef3b3f
Tfloow <88899420+Tfloow@users.noreply.github.com>
2024-03-31 13:34:24 +0200

    Data Loading optimized

--- assignment_2/deepNeuralTraining.py
+++ assignment_2/deepNeuralTraining.py
@@ -27,7 +27,7 @@ else:
 
 # Creating the basic model
 model = Sequential([
-    Dense(26, activation='relu', input_shape=(32,)),
+    Dense(26, activation='relu', input_shape=(33,)),
     Dense(16, activation='relu'),
     Dense(8, activation='relu'),
     Dense(1, activation='sigmoid')
@@ -42,61 +42,79 @@ model.summary()
 # First loading all the training data
 # Just loading a game
 if not load:
-    X = np.zeros((1, 32))
-    Y = np.zeros((1, 1))
+    X = []
+    Y = []
+    size = 0
 
     games = os.listdir(folder+"/draw")
 
     for game in games[:int(percent_of_data*len(games))]:
         data = np.load(folder+"/draw/"+game)
-        X = np.vstack([X, data])
-        tmp = np.zeros((data.shape[0], 1))
-        tmp.fill(0.5)
-        Y = np.vstack([Y,tmp])
+        turn = np.arange(data.shape[0])
+        data = np.hstack([data, turn.reshape(-1,1)])
         
-    print(X.shape)
-    print(Y.shape)
+        X = [*X,*data]
+    
+    tmp = np.zeros(len(X) - size)
+    tmp.fill(0.5)
+    Y = [*Y, *tmp]
+    size = len(X)
+    
+    print(len(X))
+    print(len(Y))
+    print(size)
 
 
     games = os.listdir(folder+"/black")
 
     for game in games[:int(percent_of_data*len(games))]:
         data = np.load(folder+"/black/"+game)
-        X = np.vstack([X, data])
-        Y = np.vstack([Y, np.ones((data.shape[0], 1))])
+        turn = np.arange(data.shape[0])
+        data = np.hstack([data, turn.reshape(-1,1)])
         
-    print(X.shape)
-    print(Y.shape)
+        X = [*X,*data]
         
+    Y = [*Y, *np.ones(len(X) - size)]
+    size = len(X)
+
+        
+    print(len(X))
+    print(len(Y))
+    print(size)
+
+    
     games = os.listdir(folder+"/white")
 
     for game in games[:int(percent_of_data*len(games))]:
         data = np.load(folder+"/white/"+game)
-        X = np.vstack([X, data])
-        Y = np.vstack([Y, np.zeros((data.shape[0], 1))])
+        turn = np.arange(data.shape[0])
+        data = np.hstack([data, turn.reshape(-1,1)])
         
-    print(X.shape)
-    print(Y.shape)
+        X = [*X,*data]
 
-    X = X[1:]
-    Y = Y[1:]
+    Y = [*Y, *np.zeros(len(X) - size)]
+    size = len(X)
+
+
+    print(len(X))
+    print(len(Y))
+    print(size)
+
+    Y = np.array(Y)
+    X = np.array(X)
 
     np.save("X.npy", X)
     np.save("Y.npy", Y)
 else:
-    X = np.load("X_big.npy")
-    Y = np.load("Y_big.npy")
+    X = np.load("X.npy")
+    Y = np.load("Y.npy")
 
-# Don't want the draw games
-finish_draw = np.max(np.argwhere(Y == 0.5))
-X = X[finish_draw:]
-Y = Y[finish_draw:]
     
 model.fit(X, Y, epochs=10, batch_size=32)
 
 
 # Saving the model
-model.save("shobu_model_binary.keras")
+model.save("shobu_model_big2.keras")
 
 # Testing the model
 # Example with a white win
