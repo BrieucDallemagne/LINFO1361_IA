ce60a2596bea4384e5af9868ccb7de096bb3c9b1
Tfloow <88899420+Tfloow@users.noreply.github.com>
2024-04-16 21:09:20 +0200

    Benchmark

--- assignment_3/code_templates_and_instances/benchmark_namazon.py
+++ assignment_3/code_templates_and_instances/benchmark_namazon.py
@@ -1,8 +1,38 @@
 from namazon import *
 import matplotlib.pyplot as plt
 
+"""
 # Benchmarking variables
-sizes = [10, 11, 12, 13, 20, 25, 30]
+sizes = [35]
 problems = [NAmazonsProblem(size) for size in sizes]
 
-compare_searchers(problems, ["nom", 10, 11, 12, 13, 20, 25, 30], [astar_search, breadth_first_graph_search, depth_first_graph_search])
+compare_searchers(problems, ["nom", 35], [astar_search])
+
+"""
+x = [10,11,12,13,14,15,20,25,30,35]
+
+# A star
+A_EN = [21,57,128,448,1527,114,746,252,144, 99]
+
+# BFS
+BFS_EN = [325, 24, 173,107,6580,2606,5181,319795]
+
+# DFS
+DFS_EN = [2298, 7149, 23923, 87922]
+
+plt.plot(x, A_EN, label="A*")
+plt.plot(x[:len(BFS_EN)], BFS_EN, label="BFS")
+plt.plot(x[:len(DFS_EN)], DFS_EN, label="DFS")
+
+# set Y log scale
+plt.yscale('log')
+plt.grid(True)
+plt.grid(True, which="minor", axis="y", ls="--")
+plt.legend()
+
+plt.title("Amount of Expanded Nodes for NAmazons")
+plt.ylabel("Amount Expanded Nodes [EN]")
+plt.xlabel("NAmazons Size [N]")
+
+plt.savefig("benchmark_namazon.pdf")
+plt.show()
